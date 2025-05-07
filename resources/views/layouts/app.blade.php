e45c5af1a7815b44a56617baa2ebd3ec45a4acd5
jihad <jihadahm79@gmail.com>
2025-05-08 02:49:07 +0300

    Add latest changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,32 +1,42 @@
 <!doctype html>
 <html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        href="{{ asset('css/app.css') }}"
         integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+        @vite('resources/css/app.css')
 </head>
 
-<body>
+<body class=".bg-light">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand " href="{{route('home')}}"><h2>Laravel</h2></a>
+            <a class="navbar-brand text-danger fw-bold fs-2" href="{{ route('home') }}">
+                Laravel
+            </a>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('posts.index') }}"><b>All Posts<b></a>
-                </ul>
 
+            <div class="collapse navbar-collapse d-flex justify-content-between align-items-center"
+                id="navbarSupportedContent">
+                <ul class="navbar-nav ms-3">
+                    <li class="nav-item">
+                        <a class="nav-link active fw-bold" aria-current="page" href="{{ route('posts.index') }}">
+                            All Posts
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
+
     <div class="container mt-5">
         @yield('content')
     </div>
